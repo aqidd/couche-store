10db9cd54588c3ecd712bf278ae3ca463cfd30d6
aqid <aa.qidd@gmail.com>
2024-06-14 14:34:07 +0700

    [update] favicon + banner

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -8,6 +8,9 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <link rel="stylesheet" href="/styles.css" />
+        <link rel="icon" href="/images/favicon.ico" />
+        <meta name="description" content="cari sprei lokal, jepang, tencel dan katun egypt dengan kualitas terbaik." />
+        <meta name="keywords" content="sprei, lokal, jepang, tencel, katun Mesir, beli sprei, sprei kasur" />
       </Head>
       <RecoilRoot>
         <ThemeProvider theme={theme}>
